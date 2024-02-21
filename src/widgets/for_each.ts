f5c9b8e09dcae45d037db85d9dd0729d229f7fb7
oboard <oboard@outlook.com>
2024-02-21 15:38:08 +0800

    feat: for_each

---
+++ src/widgets/for_each.ts
@@ -0,0 +1,34 @@
+import { BuderState } from "../state";
+import { _Builder } from "./builder";
+import { _View } from "./view";
+import { BuderWidget } from "./widget";
+
+export class _ForEach<T> extends _View {
+  _state: BuderState<Iterable<T>>;
+  _builder: (item: T, index: number) => BuderWidget;
+  constructor(
+    state: BuderState<Iterable<T>>,
+    builder: (item: T, index: number) => BuderWidget
+  ) {
+    super();
+    this._state = state;
+    this._builder = builder;
+  }
+
+  render(): HTMLElement {
+    let index = 0;
+    this._children = [];
+    for (const item of this._state.value) {
+      const builder = this._builder(item, index++);
+      this._children.push(builder);
+    }
+    return super.render();
+  }
+}
+
+export function ForEach<T>(
+  state: BuderState<Iterable<T>>,
+  builder: (item: T, index: number) => BuderWidget
+) {
+  return new _ForEach(state, builder);
+}
